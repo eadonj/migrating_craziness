40cccbf210bb7c88534aea8e11a716629812d01b
nathaniel <nathanielwroblewski@gmail.com>
2013-07-02 10:58:03 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SweetArticles::Application.config.secret_token = 'b80279218ac385d381a52bfe3a90c4882eb25ea80b86aafbba58cf3a705370dd913e07cef989523fc4803f0686a7e7c70fed0534799a8a3cbf9b02810362860a'
